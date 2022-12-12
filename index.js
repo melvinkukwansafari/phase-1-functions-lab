844886f449d6f112ed83b2b8bb6f68ff42c1ca9e
melvinkukwansafari <melvin.kukwan@student.moringaschool.com>
2022-12-12 14:36:03 +0300

    what is wrong: for a whole day

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+let pickupLocation = 42;
+function distanceFromHqInBlocks(pickupLocation) {
+    const hqLocation = 50;
+    return Math.abs(hqLocation - pickupLocation);
+  }
+  
+  
